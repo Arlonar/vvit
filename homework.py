bef0ea2e68dd219878e70a0f47019bfc7924a285
Arlonar <sarafannikov.artem9@gmail.com>
2022-09-30 13:50:59 +0300

    Fix discriminant < 0

--- homework.py
+++ homework.py
@@ -2,7 +2,12 @@ a, b, c = map(int, input().split())
 
 d = b * b - 4 * a * c
 
-x1 = (-b - d ** 0.5) / (2 * a)
-x2 = (-b + d ** 0.5) / (2 * a)
-
-print(x1, x2)
+if d < 0:
+    print('Нет корней')
+elif d == 0:
+    x = (-b - d ** 0.5) / (2 * a)
+    print(x)
+else:
+    x1 = (-b - d ** 0.5) / (2 * a)
+    x2 = (-b + d ** 0.5) / (2 * a)
+    print(x1, x2)
